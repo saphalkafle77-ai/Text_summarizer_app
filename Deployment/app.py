a0cbfff2651a804d4a86e59d838583dbac90977e
Saphal Kafle <saphalkafle77@gmail.com>
2026-07-18 18:06:14 +0545

    device and templating

--- Deployment/app.py
+++ Deployment/app.py
@@ -6,3 +6,20 @@ import re
 from fastapi.templates import Jinja2Templates  #UI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
+
+#Initalize our fastapi app
+app = FastAPI(title="Text Summarizeer App", description="Here you can summarize any long conversation or dialogue",version="1.0")
+
+#model and tokenizer import 
+model = T5ForConditionalGeneration.from_pretrained("./saved_summary_model")
+tokenizer = T5Tokenizer.from_pretrained("./saved_summary_model")
+
+#device 
+
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else: 
+    device = torch.device("cpu")
+model.to(device)
